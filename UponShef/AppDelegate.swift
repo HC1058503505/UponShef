67b72b8448c8670c77c62441484a7020bc77efee
HC1058503505 <1058503505@qq.com>
2019-02-25 13:06:12 +0800

    remove test

--- UponShef/AppDelegate.swift
+++ UponShef/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // Test
+        
         createShortcutItems()
         
         // 程序启动，点开Touch
